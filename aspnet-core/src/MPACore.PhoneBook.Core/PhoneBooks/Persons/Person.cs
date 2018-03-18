5c3c25da0d7383de5be1c598d8fbaec96e182774
桐铭 梁 <werltm@hotmail.com>
2018-03-19 00:43:52 +0800

    初始化

---
+++ aspnet-core/src/MPACore.PhoneBook.Core/PhoneBooks/Persons/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+
+namespace MPACore.PhoneBook.PhoneBooks.Persons
+{
+    /// <summary>
+    /// 人员
+    /// </summary>
+    
+    public class Person :FullAuditedEntity
+    {
+
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 邮箱
+        /// </summary>
+        [EmailAddress]
+        [MaxLength(80)]
+        public string EmailAddress { get; set; }
+
+        /// <summary>
+        /// 地址信息
+        /// </summary>
+        [MaxLength(200)]
+        public string Address { get; set; }
+
+
+
+
+
+
+
+    }
+}
