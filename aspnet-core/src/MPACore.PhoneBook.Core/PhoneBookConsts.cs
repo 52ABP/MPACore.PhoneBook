bf89550cbec6aa1e2d0c04b53a443620f7bdaf42
桐铭 梁 <werltm@hotmail.com>
2018-03-25 14:35:33 +0800

    修改部分信息

--- aspnet-core/src/MPACore.PhoneBook.Core/PhoneBookConsts.cs
+++ aspnet-core/src/MPACore.PhoneBook.Core/PhoneBookConsts.cs
@@ -7,5 +7,24 @@
         public const string ConnectionStringName = "Default";
 
         public const bool MultiTenancyEnabled = false;
+
+
+        /// <summary>
+        /// 名字最大长度
+        /// </summary>
+        public const int MaxNameLength = 32;
+        /// <summary>
+        /// Default page size for paged requests.
+        /// </summary>
+        public const int DefaultPageSize = 10;
+
+        /// <summary>
+        /// Maximum allowed page size for paged requests.
+        /// </summary>
+        public const int MaxPageSize = 1000;
+        /// <summary>
+        /// 邮件地址最大长度
+        /// </summary>
+        public const int MaxEmailAddressLength = 255;
     }
 }
